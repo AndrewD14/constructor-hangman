a98216d7d66dc6a92fd629d65b29ed9edf5f587e
AndrewD14 <Leingod2005@gmail.com>
2017-11-18 12:05:44 -0600

    Modify to use the random phrase logic.

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var Word = require('./js/word.js');
 var Player = require('./js/player.js');
 var Inputs = require('./js/inputs.js');
 var Display = require('./js/display.js');
-var Phrase = require('./js/phrases.js')
+var Phrase = require('./js/phrases.js');
 
 //general global variables
 var myWord = new Word(Phrase.getRandomPhrase());
